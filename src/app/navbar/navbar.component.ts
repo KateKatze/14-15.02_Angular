2cb001dc8eabbb4cfdbffc58aac04cebf48a82a5
KateKatze <dr.katekogler@gmail.com>
2022-02-15 11:36:19 +0100

    15.02 Exercises done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  clickCounter = 10;
+  countClick() {
+    this.clickCounter += 10;
+    console.log(this.clickCounter);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
